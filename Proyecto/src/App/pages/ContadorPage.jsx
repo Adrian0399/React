33af61cf3ac0e8aab49068857cfa3ee18f023172
AdrianKlu <84831257+Adrian0399@users.noreply.github.com>
2023-02-21 16:21:05 -0600

    2do commit
    Add navbar

--- Proyecto/src/App/pages/ContadorPage.jsx
+++ Proyecto/src/App/pages/ContadorPage.jsx
@@ -15,14 +15,14 @@ export function ContadorPage() {
 
   return (
     <div className="App">
-      <div>
-        {/* <a href="https://vitejs.dev" target="_blank">
+        {/* <div>
+        <a href="https://vitejs.dev" target="_blank">
           <img src="../../assets/react.svg" className="logo" alt="Vite logo" />
         </a>
         <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a> */}
-        </div>
+          <img src={ reactLogo } className="logo react" alt="React logo" />
+        </a>
+        </div> */}
       <h1>Vite + React</h1>
 
       <p>Valor del count: { count.value }</p>
@@ -58,10 +58,7 @@ export function ContadorPage() {
         </p>
       </div>
 
-        <NavLink
-        to="/formulario">
-          Formulario
-        </NavLink>
+        
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
