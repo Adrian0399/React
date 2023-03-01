0012a8c119b14eae95df833e6144d5547b96642f
AdrianKlu <84831257+Adrian0399@users.noreply.github.com>
2023-03-01 16:29:23 -0600

    1.7.1
    Añade Icono dentro de botones de Menu

--- new-project/src/components/tarjetasCoporativas/TarjetasCorporativasView.jsx
+++ new-project/src/components/tarjetasCoporativas/TarjetasCorporativasView.jsx
@@ -33,6 +33,7 @@ export const TarjetasCorporativasView = () => {
                 onClick={ handleMisTarjetasClick }
                 value="MisTarjetasView"
               >
+                <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M2.99847 18.9997L1.10111 6.28367C1.01789 5.7259 1.41209 5.20968 1.97209 5.14309L19.0509 3.11233C19.5812 3.04927 20.0675 3.4141 20.1553 3.94093L20.9985 8.99966" stroke="#2F2F2F" stroke-width="1.5"></path><mask id="path-2-inside-1_7326_88239" fill="white"><rect x="2" y="8" width="20" height="12" rx="1"></rect></mask><rect x="2" y="8" width="20" height="12" rx="1" stroke="#2F2F2F" stroke-width="3" mask="url(#path-2-inside-1_7326_88239)"></rect><path d="M3 12H21" stroke="#2F2F2F" stroke-width="1.5"></path><circle cx="17.5" cy="16.5" r="0.75" fill="#2F2F2F" stroke="#2F2F2F" stroke-width="1.5"></circle><circle cx="18.5" cy="16.5" r="0.75" fill="#2F2F2F" stroke="#2F2F2F" stroke-width="1.5"></circle><path d="M5 14C5 14 6.21895 14 7 14" stroke="#2F2F2F" stroke-width="1.5" stroke-linecap="round"></path><path d="M5 16C5 16 7.4379 16 9 16" stroke="#2F2F2F" stroke-width="1.5" stroke-linecap="round"></path></svg>    
                 Mis Tarjetas Corporativas
               </button>
               <button 
@@ -40,6 +41,7 @@ export const TarjetasCorporativasView = () => {
                 onClick={ handleSolicitudesClick }
                 value="SolicitudesView"
               >
+                <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M2.99847 18.9997L1.10111 6.28367C1.01789 5.7259 1.41209 5.20968 1.97209 5.14309L19.0509 3.11233C19.5812 3.04927 20.0675 3.4141 20.1553 3.94093L20.9985 8.99966" stroke="#2F2F2F" stroke-width="1.5"></path><mask id="path-2-inside-1_7326_88239" fill="white"><rect x="2" y="8" width="20" height="12" rx="1"></rect></mask><rect x="2" y="8" width="20" height="12" rx="1" stroke="#2F2F2F" stroke-width="3" mask="url(#path-2-inside-1_7326_88239)"></rect><path d="M3 12H21" stroke="#2F2F2F" stroke-width="1.5"></path><circle cx="17.5" cy="16.5" r="0.75" fill="#2F2F2F" stroke="#2F2F2F" stroke-width="1.5"></circle><circle cx="18.5" cy="16.5" r="0.75" fill="#2F2F2F" stroke="#2F2F2F" stroke-width="1.5"></circle><path d="M5 14C5 14 6.21895 14 7 14" stroke="#2F2F2F" stroke-width="1.5" stroke-linecap="round"></path><path d="M5 16C5 16 7.4379 16 9 16" stroke="#2F2F2F" stroke-width="1.5" stroke-linecap="round"></path></svg>    
                 Solicitar Tarjetas
               </button>
             </div>
